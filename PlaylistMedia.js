d14505d16d7e7d91de576a678dc790a7a2c43636
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2021-04-03 08:07:18 +0200

    Add logic to preload files before playing

--- PlaylistMedia.js
+++ PlaylistMedia.js
@@ -14,16 +14,57 @@ class PlaylistMedia {
    */
   constructor (list, options) {
     this.list = list
-    this.index = 0
+    this.preloadIndex = 0
     this.options = options
     this.dom = this.options.dom
+
+    this.preloadList = []
+    this.current = null
+
+    this.preloadListInit()
+    this.preload()
+
     this.videos = [document.createElement('video')]
     this.videos.forEach(video => {
-      video.onended = () => this.next()
     })
     this.audios = [new Audio()]
     this.audios.forEach(video => {
       video.onended = () => this.next()
+    })
+  }
+
+  preloadListInit () {
+    for (let i = 0; i < 1; i++) {
+      const entry = {
+        index: null,
+        video: document.createElement('video'),
+        audio: new Audio()
+      }
+
+      entry.video.onended = () => this.next()
+      entry.audio.onended = () => this.next()
+      entry.video.controls = true
+
+      this.preloadList.push(entry)
+    }
+  }
+
+  preload () {
+    this.preloadList.forEach(preload => {
+      if (preload.index !== null) {
+        return
+      }
+
+      preload.index = this.preloadIndex++
+      if (preload.index >= this.list.length) {
+        preload.index = null
+        return
+      }
+
+      const entry = this.list[preload.index]
+
+      preload.video.src = entry.video || null
+      preload.audio.src = entry.audio || null
     })
   }
 
@@ -35,22 +76,32 @@ class PlaylistMedia {
       this.dom.removeChild(this.dom.firstChild)
     }
 
+    this.current = this.preloadList.shift()
+
+    this.index = this.current.index
+    if (this.index === null) {
+      return
+    }
+
     const entry = this.list[this.index]
+
     if (entry.video) {
-      this.options.dom.appendChild(this.videos[0])
-      this.videos[0].src = entry.video
-      this.videos[0].play()
+      this.options.dom.appendChild(this.current.video)
+      this.current.video.play()
     }
     if (entry.audio) {
-      this.audios[0].src = entry.audio
-      this.audios[0].play()
+      this.current.audio.play()
     }
   }
 
   next () {
-    this.index++
-    if (this.index < this.list.length) {
-      this.play()
+    if (this.current) {
+      this.current.index = null
+      this.preloadList.push(this.current)
+      this.current = null
     }
+
+    this.preload()
+    this.play()
   }
 }
